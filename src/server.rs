17cd1c207c251234cab0fe82467342dac62bc425
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-08-17 20:17:59 -0300

    refactor: simplify `server.rs` imports by removing redundant `bash_state` module path

--- src/server.rs
+++ src/server.rs
@@ -6,7 +6,7 @@ use std::sync::Arc;
 use tokio::sync::Mutex;
 use tracing::info;
 
-use crate::state::bash_state::BashState;
+use crate::state::BashState;
 
 /// Winx service with shared bash state
 #[derive(Clone)]
